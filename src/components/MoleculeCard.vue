c8c629b6047633d48ec4a559b0d18e4804715d04
anthonyophernandez <anthonyophernandez@gmail.com>
2020-07-02 11:23:16 +0200

    Stats Component Creation, Stats Cards

---
+++ src/components/MoleculeCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="p-4 mb-2 border border-blue-200 hover:shadow-lg rounded-sm bg-blue-100 text-blue-800">
+    <AtomTitle tag="h2" :content="card.title" />
+    <AtomText :text="card.text" />
+  </div>
+</template>
+
+<script>
+import AtomTitle from '../components/AtomTitle.vue'
+import AtomText from '../components/AtomText.vue'
+
+export default {
+  name: 'MoleculeCard',
+  props: ['card'],
+  components: {
+    AtomTitle,
+    AtomText
+  }
+}
+</script>
---
+++ src/views/Stats.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="p-2">
+
+    <OrganismGrid :cards="cards" />
+
+  </section>
+</template>
+
+<script>
+import OrganismGrid from '../components/OrganismGrid.vue'
+export default {
+  name: 'Stats',
+  components: {
+    OrganismGrid
+  },
+  data: () => {
+    return {
+      cards: [
+        {
+          title: 'Overall Spent',
+          text: '€ 34.288,34'
+        },
+        {
+          title: 'Most Spent By',
+          text: 'Debit Card'
+        },
+        {
+          title: 'Most Spent On',
+          text: 'Shopping'
+        },
+        {
+          title: 'Spent This Year',
+          text: '€ 15.233,85'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
