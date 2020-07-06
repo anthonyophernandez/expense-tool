0b30e19b3bfd4c1d46327bfbb18e5871e94f4d7d
anthonyophernandez <anthonyophernandez@gmail.com>
2020-07-06 10:38:56 +0200

    Axios & MirageJs Installation & Setup

--- src/components/OrganismTableAndChart.vue
+++ src/components/OrganismTableAndChart.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="flex flex-col w-full h-full border border-blue-200 rounded-sm bg-blue-100 p-1 mb-4">
 
-    <div class="px-4 w-full flex items-end">
+    <div class="px-4 pb-2 mb-4 w-full border-b border-blue-200 flex items-end">
       <AtomTitle class="text-lg text-blue-500 pt-2 pb-1 mr-2" tag="h3" :content="title"/>
       <div class="pt-2 pb-1 flex">
         <MoleculeSelect class="mr-2" :label="byYear.label" :options="byYear.data"/>
@@ -12,11 +12,11 @@
     <div class="flex flex-wrap items-stretch w-full h-full">
       <div class="w-full lg:w-1/2 min-h-full flex flex-col justify-between">
 
-        <div class="overflow-x-auto">
-          <OrganismTable class="w-full" :table="table"/>
+        <div class="overflow-x-auto h-40 mb-2">
+          <OrganismTable class="w-full h-56" :table="table"/>
         </div>
 
-        <div class="flex items-center justify-end">
+        <div class="flex items-center border-t border-blue-200 justify-end">
           <AtomText class="text-xs mr-2" text="1-5 of 5"/>
           <AtomButton class="rounded-l" content="<" />
           <AtomButton class="rounded-r" content=">" />
---
+++ src/mirage/server-setup.js
@@ -0,0 +1,12 @@
+import { Server } from 'miragejs'
+import typesJSON from './types.json'
+
+export default function () {
+  const server = new Server()
+
+  server.namespace('api')
+
+  server.get('/types', ({ db }, request) => db.types)
+
+  server.db.loadData({ types: typesJSON })
+}
